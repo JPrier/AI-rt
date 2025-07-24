442521b4f3be645f13944cc48f397b637d5a0832
Josh <24302717+JPrier@users.noreply.github.com>
2025-07-24 16:48:43 -0700

    Add cat ASCII art function

--- art.py
+++ art.py
@@ -1,7 +1,15 @@
-print(
-    r"""
+
+"""Module providing ASCII art."""
+
+
+def get_cat_art() -> str:
+    """Return a simple cat ASCII art."""
+    return r"""
   /\_/\
  ( o.o )
   > ^ <
 """
-)
+
+
+if __name__ == "__main__":
+    print(get_cat_art())
